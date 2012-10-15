2212cb57b61e1ae940bfb7d2f4ce202b3dec7126
Santiago Lopez <santiago.lopez.pina@gmail.com>
2012-10-15 23:45:07 +0200

    First commit

---
+++ src/dimensions.h
@@ -0,0 +1,13 @@
+#ifndef DIMENSIONS_H
+#define DIMENSIONS_H
+//- Define box dimensions in mm
+#define BOX_X 180
+#define BOX_Y 200
+#define BOX_Z 30
+#define THCK 10
+
+//- Define dimensions in mm
+#define GLASS_X 50
+#define GLASS_Y 50
+#define GLASS_Z 10
+#endif
