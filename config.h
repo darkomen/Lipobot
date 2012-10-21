49e8c3e9c7b98c2bbf79cfd7f221744b02fa2ebf
Santiago Lopez <santiago.lopez.pina@gmail.com>
2012-10-21 14:33:40 +0200

    Fichero con las medidas del robot

---
+++ config.h
@@ -0,0 +1,12 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+	
+float LX;    					// Ancho del robot (eje X)
+#define LY 93			    	// Largo del robot (eje Y)
+#define LZ 23	 				// Alto del robot (eje Z)
+#define TH 5	 				// Ancho de la pared 
+#define LX_servo 27 			// Ancho del servo (eje X)
+#define LY_servo 40  			// Larego del servo (eje Y)
+#define LZ_servo 20  			// alto del servo (eje Z)
+#define DX_servos 3 			// Distancia entre ambos servosååå
+#endif
